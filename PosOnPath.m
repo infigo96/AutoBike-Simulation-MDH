af7480c818d53109d10d062965bcc302bde0acba
Unknown <hbz15001@student.mdh.se>
2020-03-18 18:00:45 +0100

    Mathematics fault fixed

--- PosOnPath.m
+++ PosOnPath.m
@@ -13,7 +13,7 @@ for j= 1:16
     hold on;
     xlim([4 8])
     ylim([4 8])
-    n = [sin((deg2rad(angle(j)+90))) -cos((deg2rad(angle(j)+90)))];
+    n = [cos(deg2rad(angle(j))) sin(deg2rad(angle(j)))];
 
     for i = 1:8
         usnit = dot([bikePos(i,1)-curvepos(1), bikePos(i,2)-curvepos(2)],n); 
@@ -29,14 +29,14 @@ end
 
 %Same as in Simulink choosing where the bicycle is on the planned path so
 %the bicycle have a correct point on the path to measure the error. 
-%Also requered when lateral control is implemented on a real bicycle.
+%Also required when lateral control is implemented on a real bicycle.
 function [y,io] = SimulinkPosChooser(path,bike,i)
     j = i;
     usnit = 1;
     y = path(j,:);
     io = 1;
     while(j < length(path) && usnit >= 0)
-        n = [sin((path(j,3)+pi/2)) -cos((path(j,3)+pi/2))];
+        n = [cos(path(j,3)) sin(path(j,3))];
         usnit = dot([bike(1)-path(j,1), bike(2)-path(j,2)],n); 
         if(usnit >= 0)
            y =  path(j,:);
